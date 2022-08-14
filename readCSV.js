e2dadb90388cf8ef5e1c1e502c95307c8b5b4e71
mahadikpranav <111154503+mahadikpranav@users.noreply.github.com>
2022-08-15 02:20:32 +0530

    skip empty lines

--- readCSV.js
+++ readCSV.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
-const book = require('./model/book.model');
+const book = require("./model/book.model");
 
 function getDataFromCSV(filePath) {
   const data = [];
   // "./data/authors.csv"
   return new Promise((resolve, reject) => {
-  fs.createReadStream(filePath)
-    .pipe(parse({ columns:true, delimiter: ";",trim: true}))
-    .on("data", (row) => {
+    fs.createReadStream(filePath)
+      .pipe(parse({ columns: true, delimiter: ";", trim: true, skip_empty_lines: tru,skip_records_with_empty_values: true }))
+      .on("data", (row) => {
         data.push(row);
-    })
-    .on("end", () => {
-      console.log(data);
-      resolve(data);
-    })
-    .on("error", (error) => {
-      console.log(error.message);
-      reject(error);
-    });
+      })
+      .on("end", () => {
+        console.log(data);
+        resolve(data);
+      })
+      .on("error", (error) => {
+        console.log(error.message);
+        reject(error);
+      });
   });
 }
 
 module.exports = {
-  getDataFromCSV
+  getDataFromCSV,
 };
